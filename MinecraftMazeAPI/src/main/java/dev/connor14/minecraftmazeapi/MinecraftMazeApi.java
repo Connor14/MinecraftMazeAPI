cfec20f6736f21dfcf0b39549123769aa95bec1c
Connor Schmidt <schmidty68connor@gmail.com>
2019-12-05 23:46:23 -0600

    generated Spigot 1.14.4 plugin

---
+++ MinecraftMazeAPI/src/main/java/dev/connor14/minecraftmazeapi/MinecraftMazeApi.java
@@ -0,0 +1,17 @@
+package dev.connor14.minecraftmazeapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftMazeApi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
